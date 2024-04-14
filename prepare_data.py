c8ead08b3d07d42e8ce34f4f3775fd6c41921c74
ronaldzhao17 <ronaldzhao17@gmail.com>
2024-04-14 15:36:33 -0700

    delete bad images
    removed images that did not show full body

--- prepare_data.py
+++ prepare_data.py
@@ -2,7 +2,7 @@ import preprocessor
 from preprocessor import cv2
 
 FILECOUNT = 78
-for i in range(1, FILECOUNT+1):
+for i in range(49, FILECOUNT+1):
     image = cv2.imread("training_images/%s.jpg"%(i))
     try:
         preprocessor.preprocess(image, "training_set/%s.png"%(i))
