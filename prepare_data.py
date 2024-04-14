7ecf0b3a19ef570bcaa3492147cbc1b41ddb8f52
Erik Shen <159072517+qwertyblurby@users.noreply.github.com>
2024-04-14 14:16:40 -0700

    changed some stuff

--- prepare_data.py
+++ prepare_data.py
@@ -1,10 +1,11 @@
 import preprocessor
 from preprocessor import cv2
-preprocessor.preprocess(cv2.imread("training_images/13.jpg"), "training_set/13.png")
 
-'''
 FILECOUNT = 87
 for i in range(11, FILECOUNT+1):
     image = cv2.imread("training_images/%s.jpg"%(i))
-    preprocessor.preprocess(image, "training_set/%s.png"%(i))
-    print("Processed image %s"%(i))'''
+    try:
+        preprocessor.preprocess(image, "training_set/%s.png"%(i))
+        print("Processed image %s"%(i))
+    except Exception as e:
+        print("Error with image %s"%(i))
