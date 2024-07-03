c69eff709b90316cebe498233c92f9ca4b5e767e
berrybear06 <79681277+berrybear06@users.noreply.github.com>
2024-07-02 20:20:58 -0700

    Fix case of URL
    Also add a disclaimer

--- fitgpt-site/src/SectionTech.js
+++ fitgpt-site/src/SectionTech.js
@@ -22,7 +22,11 @@ function SectionTech() {
 	return (
 		<div className="space-y-6">
 			<h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">Technical Details</h2>
-			<p className="text-gray-400">We reduce the task of outfit recommendations to a set of image classification problems, simplifying the AI architecture involved.</p>
+			<p className="text-gray-400">
+				We reduce the task of outfit recommendations to a set of image classification problems, simplifying the AI architecture involved.
+				<br /> <br />
+				Note: despite our product's name, we do not use a generative pre-trained transformer.
+			</p>
 			<div className="grid grid-cols-1 gap-6 sm:grid-cols-1 lg:grid-cols-3">
 				{floaters.map(([titleText, description, iconSrc]) => (
 					<Floater
